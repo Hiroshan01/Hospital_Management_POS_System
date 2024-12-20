eaeffc4603aa3548b43f4f8abab716924c245a61
Hiroshan Madusanka <pmhiroshan@gmail.com>
2024-12-20 20:33:14 +0530

    ss

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
+
+from flask_mysqldb import MySQL
 from models import user_data #model file in user_data.py
 from typing import Any
 from models import items
--- config.py
+++ config.py
@@ -4,3 +4,7 @@ class Config:
     MYSQL_PASSWORD = ''
     MYSQL_DB = 'hospital_management_system'
     SECRET_KEY = 'Hiroshan1999'
+
+
+    # SQLAlchemy Database URI
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
